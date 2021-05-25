b9a97255a7c24f271afe920b13764021e7cfbcb6
alex98102011 <alex98102011@hotmail.com>
2021-05-24 20:17:37 -0500

    No se JSON ayudaaaaa

--- app/src/main/java/com/example/mysge/adapters/RecyclerInscripcionCalificacionAdapter.kt
+++ app/src/main/java/com/example/mysge/adapters/RecyclerInscripcionCalificacionAdapter.kt
@@ -13,11 +13,15 @@ import com.example.mysge.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.json.JSONArray
 import org.json.JSONObject
+import android.os.Bundle
+import android.content.Intent
+import android.provider.Settings.Global.getString
 
 class RecyclerInscripcionCalificacionAdapter(val c: Context, val r: Int, val calificaciones: JSONArray) : RecyclerView.Adapter<RecyclerInscripcionCalificacionAdapter.CalificacionInscripcionVH>()  {
 
 
     inner class CalificacionInscripcionVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         fun bind(json : JSONObject) {
             val tvMateria = itemView.findViewById<TextView>(R.id.tvRowMateriaInscripcion)
             val tvCalificacion = itemView.findViewById<TextView>(R.id.tvRowCalificacionInscripcion)
@@ -25,8 +29,6 @@ class RecyclerInscripcionCalificacionAdapter(val c: Context, val r: Int, val cal
             val btnSelect=itemView.findViewById<FloatingActionButton>(R.id.btnSeleccionarMateia)
             val btnPr1=itemView.findViewById<RadioButton>(R.id.Profesor1)
             val btnPr2=itemView.findViewById<RadioButton>(R.id.Profesor2)
-
-
             tvMateria.text = json.getString("materia")
             if( ! json.getString("calificacion").equals("-")) {
                 tvCalificacion.text = json.getString("calificacion")
@@ -46,24 +48,18 @@ class RecyclerInscripcionCalificacionAdapter(val c: Context, val r: Int, val cal
                 cardView.setCardBackgroundColor(Color.BLUE)
                 tvCalificacion.text = "Materia Posible a Seleccionar"
             }
-            var i: Int
-            i=0
 
-                btnSelect.setOnClickListener {
-                    cardView.setCardBackgroundColor(Color.RED)
-                    tvCalificacion.text = "Cursando"
-                    i++
-                    print(i)
-                }
+            btnSelect.setOnClickListener {
+                cardView.setCardBackgroundColor(Color.RED)
+                tvCalificacion.text = "Cursando"
 
-
-
-
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalificacionInscripcionVH {
         return CalificacionInscripcionVH( LayoutInflater.from(c).inflate(r,null) )
+
     }
 
     override fun onBindViewHolder(holder: CalificacionInscripcionVH, position: Int) {
